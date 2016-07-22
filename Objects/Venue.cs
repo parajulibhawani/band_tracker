2014e411cc8682fcbd43fa4dca0dd259ad3903c3
Bhawani Parajuli <parajulibhawani@gmail.com>
2016-07-22 14:53:58 -0700

    add test method to check empty datase

---
+++ Objects/Venue.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Venue
+  {
+    private int _id;
+    private string _venueName;
+
+    public Venue(string venueName, int id = 0)
+    {
+      _id = id;
+      _venueName = venueName;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetVenueName()
+    {
+      return _venueName;
+    }
+    public override bool Equals(System.Object otherVenue)
+    {
+      if (!(otherVenue is Venue))
+      {
+        return false;
+      }
+      else
+      {
+        Venue newVenue = (Venue) otherVenue;
+        bool idEquality = this.GetId() == newVenue.GetId();
+        bool nameEquality = this.GetVenueName() == newVenue.GetVenueName();
+        return (idEquality && nameEquality);
+      }
+    }
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int venueId = rdr.GetInt32(0);
+        string venueName = rdr.GetString(1);
+        Venue newVenue = new Venue(venueName, venueId);
+        allVenues.Add(newVenue);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allVenues;
+    }
+  }
+}
---
+++ Tests/VenueTest.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class VenueTest : IDisposable
+  {
+    public VenueTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      int result = Venue.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+    public void Dispose()
+   {
+    //  Venue.DeleteAll();
+   }
+  }
+}
